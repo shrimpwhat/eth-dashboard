5defad705f9dcfe6536e34c2798ef5711d2912dc
Yuriy Kulakov <yuriy.kulakov.123@gmail.com>
2022-10-20 12:28:20 +0300

    now token page displays name, symbol and balance

--- frontend/src/pages/TokenPage.tsx
+++ frontend/src/pages/TokenPage.tsx
@@ -1,7 +1,9 @@
+import { ethers } from "ethers";
 import { useParams } from "react-router-dom";
 import { useContractReads, useSigner, useAccount } from "wagmi";
 import abi from "../utils/abi/ERC20.json";
 import Title from "../utils/components/Title";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function TokenPage() {
   const { address: contractAddress } = useParams();
@@ -30,14 +32,32 @@ export default function TokenPage() {
     ],
   });
 
-  if (isFetching)
-    return <h1 className="text-2xl font-bold text-center">Fetching data...</h1>;
+  if (isFetching || !address)
+    return (
+      <>
+        <Title text={"Token page"} />
+        {address ? (
+          <h1 className="text-2xl font-bold text-center">Fetching data...</h1>
+        ) : (
+          <div className="max-w-max mx-auto">
+            <ConnectButton />
+          </div>
+        )}
+      </>
+    );
   else
     return (
       <div>
         <Title text={"Token page"} />
-        <div className="card text-lg">
-          <h1 className="text-2xl">{data?.at(0)}</h1>
+        <div className="card text-xl">
+          <h1 className="text-2xl font-semibold">{data?.at(0)}</h1>
+          <p className="text-left">
+            Balance:{" "}
+            <span className="italic text-blue-500 font-bold">
+              {ethers.utils.formatEther(data?.at(2) ?? 0)}
+            </span>{" "}
+            <span className="font-bold">{data?.at(1)}</span>
+          </p>
         </div>
       </div>
     );
