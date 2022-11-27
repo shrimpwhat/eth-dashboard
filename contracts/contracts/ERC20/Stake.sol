bdb5b49885c94bc13f974413b22d2c19a318d030
Yuriy Kulakov <yuriy.kulakov.123@gmail.com>
2022-11-27 20:58:17 +0300

    updated staking contract and added tests for it

---
+++ contracts/contracts/ERC20/Stake.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/*
+    Based on:
+    https://github.com/beefyfinance/beefy-contracts/blob/master/contracts/BIFI/infra/BeefyRewardPool.sol
+    https://solidity-by-example.org/defi/staking-rewards/
+*/
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract StakingPool is Ownable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20Metadata;
+    using SafeERC20 for IERC20;
+
+    IERC20Metadata public immutable stakedToken;
+    uint256 public duration;
+    uint256 public totalSupply;
+    uint256 public periodFinish;
+    uint256 public rewardRate;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint) public balanceOf;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    constructor(address _stakedToken, uint _duration) {
+        stakedToken = IERC20Metadata(_stakedToken);
+        duration = _duration;
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(10 ** stakedToken.decimals())
+                    .div(totalSupply)
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+            balanceOf[account]
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(10 ** stakedToken.decimals())
+                .add(rewards[account]);
+    }
+
+    function stake(uint256 amount) public updateReward(msg.sender) {
+        require(amount > 0, "Cannot stake 0");
+        stakedToken.safeTransferFrom(msg.sender, address(this), amount);
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public updateReward(msg.sender) {
+        require(amount > 0, "Cannot withdraw 0");
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        stakedToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        require(balanceOf[msg.sender] > 0, "Nothing to withdraw");
+        withdraw(balanceOf[msg.sender]);
+        getReward();
+    }
+
+    function getReward() public updateReward(msg.sender) {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            stakedToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function setRewardsDuration(uint _duration) external onlyOwner {
+        require(periodFinish < block.timestamp, "reward duration not finished");
+        duration = _duration;
+    }
+
+    function notifyRewardAmount(
+        uint256 reward
+    ) external onlyOwner updateReward(address(0)) {
+        require(reward > 0, "reward = 0");
+        stakedToken.safeTransferFrom(msg.sender, address(this), reward);
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward.div(duration);
+        } else {
+            uint256 remaining = periodFinish.sub(block.timestamp);
+            uint256 leftover = remaining.mul(rewardRate);
+            rewardRate = reward.add(leftover).div(duration);
+        }
+        require(
+            rewardRate * duration <=
+                stakedToken.balanceOf(address(this)) - totalSupply,
+            "reward amount > balance"
+        );
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(duration);
+        emit RewardAdded(reward);
+    }
+
+    function inCaseTokensGetStuck(address _token) external onlyOwner {
+        require(_token != address(stakedToken), "!staked");
+        require(_token != address(stakedToken), "!reward");
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).safeTransfer(msg.sender, amount);
+    }
+}
---
+++ contracts/test/ERC20Staking.ts
@@ -0,0 +1,120 @@
+import { ethers } from "hardhat";
+import { loadFixture, time } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
+import { Contract, BigNumber } from "ethers";
+
+describe("ERC20 staking", () => {
+  const SECS_PER_YEAR = 365 * 24 * 60 * 60;
+  const signer1Balance = ethers.utils.parseEther("10000");
+  const signer2Balance = ethers.utils.parseEther("100");
+  const rewardAmount = ethers.utils.parseEther("1000");
+
+  const setup = async (duration = SECS_PER_YEAR) => {
+    const [signer1, signer2] = await ethers.getSigners();
+    const _token = await ethers.getContractFactory("Token");
+    const token = await _token.deploy("MyToken", "MTKN", signer1Balance);
+    await token.deployed();
+
+    const _staking = await ethers.getContractFactory("StakingPool");
+    const staking = await _staking.deploy(token.address, duration);
+    await staking.deployed();
+
+    await token.mintTo(signer2.address, signer2Balance);
+    await token.approve(staking.address, ethers.constants.MaxUint256);
+    await token
+      .connect(signer2)
+      .approve(staking.address, ethers.constants.MaxUint256);
+
+    await staking.notifyRewardAmount(rewardAmount);
+    return { token, staking, signer1, signer2 };
+  };
+
+  const setupFourYearDuration = async () => {
+    return await setup(SECS_PER_YEAR * 4);
+  };
+
+  const getExpectedAPR = async (stakingContract: Contract) => {
+    const rate = parseFloat(await stakingContract.rewardRate());
+    const totalStaked = parseFloat(await stakingContract.totalSupply());
+    return (rate * SECS_PER_YEAR) / totalStaked;
+  };
+
+  const getProfit = (amount: BigNumber, duration: number, apr: number) => {
+    const depositAmount = parseFloat(amount.toString());
+    return depositAmount * (1 + (apr * duration) / SECS_PER_YEAR);
+  };
+
+  it("Should stake some amount of tokens and withdraw it", async () => {
+    const { staking, signer1, token } = await loadFixture(setup);
+    const amount = ethers.utils.parseEther("100");
+    await staking.stake(amount);
+    expect(await staking.totalSupply()).eq(amount);
+    expect(await staking.balanceOf(signer1.address)).eq(amount);
+    expect(await token.balanceOf(signer1.address)).eq(
+      signer1Balance.sub(amount).sub(rewardAmount)
+    );
+    await staking.withdraw(amount);
+    expect(await staking.totalSupply()).eq(0);
+    expect(await staking.balanceOf(signer1.address)).eq(0);
+    expect(await token.balanceOf(signer1.address)).eq(
+      signer1Balance.sub(rewardAmount)
+    );
+  });
+
+  it("Should claims rewards", async () => {
+    const { token, staking, signer2 } = await loadFixture(setup);
+    const staking2 = staking.connect(signer2);
+    const amount = signer2Balance;
+    await staking2.stake(amount);
+    await time.increase(3600);
+    const earned = await staking2.earned(signer2.address);
+    await staking2.getReward();
+    expect(await token.balanceOf(signer2.address)).gt(
+      signer2Balance.sub(amount).add(earned)
+    );
+  });
+
+  it("Should exit staking pool", async () => {
+    const { token, staking, signer2 } = await loadFixture(setup);
+    const staking2 = staking.connect(signer2);
+    await staking2.stake(ethers.utils.parseEther("100"));
+    await time.increase(3600);
+    const earned = await staking2.earned(signer2.address);
+    await staking2.exit();
+    expect(await token.balanceOf(signer2.address)).gte(
+      signer2Balance.add(earned)
+    );
+  });
+
+  it("Should get rewards according to calculated APR", async () => {
+    const { token, staking, signer2 } = await loadFixture(setup);
+    const duration = SECS_PER_YEAR / 4;
+    const staking2 = staking.connect(signer2);
+    const amount = signer2Balance;
+    await staking2.stake(amount);
+    const apr = await getExpectedAPR(staking);
+    await time.increase(duration);
+    await staking2.exit();
+    const balance = parseFloat(
+      (await token.balanceOf(signer2.address)).toString()
+    );
+    expect(balance).closeTo(getProfit(signer2Balance, duration, apr), 1e17);
+  });
+
+  it("Should get rewards according to calculated APR with different duration", async () => {
+    const { token, staking, signer2 } = await loadFixture(
+      setupFourYearDuration
+    );
+    const duration = SECS_PER_YEAR * 1.5;
+    const staking2 = staking.connect(signer2);
+    const amount = signer2Balance;
+    await staking2.stake(amount);
+    const apr = await getExpectedAPR(staking);
+    await time.increase(duration);
+    await staking2.exit();
+    const balance = parseFloat(
+      (await token.balanceOf(signer2.address)).toString()
+    );
+    expect(balance).closeTo(getProfit(signer2Balance, duration, apr), 1e17);
+  });
+});
