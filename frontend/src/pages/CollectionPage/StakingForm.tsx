b358f231a0711e91d8554c6262822a990505459c
Yuriy Kulakov <yuriy.kulakov.123@gmail.com>
2023-01-30 00:12:36 +0300

    added graph schema and manifest for indexing nfts metadata

--- frontend/src/pages/CollectionPage/StakingForm.tsx
+++ frontend/src/pages/CollectionPage/StakingForm.tsx
@@ -1,14 +1,7 @@
 import { useParams } from "react-router-dom";
 import { ethers } from "ethers";
-import {
-  useContractRead,
-  useAccount,
-  useContract,
-  useSigner,
-  useContractReads,
-  useContractInfiniteReads,
-  paginatedIndexesConfig,
-} from "wagmi";
+import { useContractRead, useAccount, useContract, useSigner } from "wagmi";
+import { useState, useEffect } from "react";
 import Collection from "../../utils/abi/Collection";
 import NftStakingABI from "../../utils/abi/NftStaking";
 import NftStakingFactoryABI from "../../utils/abi/NftStakingFactory";
@@ -18,6 +11,7 @@ import { FormContainer, TextFieldElement } from "react-hook-form-mui";
 import FieldsWrapper from "../../utils/components/FieldsWrapper";
 import { txAlert } from "../../utils/components/Popups";
 import SubmitButton from "../../utils/components/SubmitButton";
+import axios from "axios";
 
 const factoryAddress = process.env.REACT_APP_NFT_STAKING_FACTORY;
 
@@ -108,24 +102,81 @@ const OwnedNFTs = ({
   contractAddress?: `0x${string}`;
 }) => {
   const { address } = useAccount();
-  const { data: tokens } = useContractRead({
+
+  const collectionContractData = {
     address: contractAddress,
     abi: Collection,
-    functionName: "tokensOfOwner",
-    args: [address ?? ethers.constants.AddressZero],
-    watch: true,
+  };
+
+  const { data: baseURI } = useContractRead({
+    ...collectionContractData,
+    functionName: "baseURI",
+    select: (data) => {
+      if (data.slice(0, 4) === "ipfs")
+        data = `https://ipfs.io/ipfs/${data.slice(7)}`;
+      return data;
+    },
   });
 
-  // const {data} = useContractInfiniteReads({
-  //   cacheKey: 'ownedNFT',
-  //   ...paginatedIndexesConfig(
-  //     (index) => {
-  //       return {
+  const { data: tokenIds } = useContractRead({
+    ...collectionContractData,
+    functionName: "tokensOfOwner",
+    args: [address ?? ethers.constants.AddressZero],
+    // watch: true,
+  });
 
+  // const [tokens, setTokens] = useState<{ name: string; image: string }[]>([]);
+  // const [isFetching, setIsFetching] = useState(true);
+
+  // useEffect(() => {
+  //   if (tokenIds?.length && baseURI) {
+  //     const fetchTokens = async () => {
+  //       const tokens = [];
+  //       for (const tokenId of tokenIds) {
+  //         const { data } = await axios.get(`${baseURI}${tokenId}`);
+  //         tokens.push(data);
   //       }
-  //     },
+  //       setTokens(tokens);
+  //       setIsFetching(false);
+  //       console.log(tokens);
+  //     };
+  //     fetchTokens();
+  //   }
+  // }, [tokenIds?.length]);
 
-  //   )
-  //   })
-  return <p>{tokens?.length}</p>;
+  // const images = [
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/123.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/73ho6zl4zyc.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/7Gikd49bzRA.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/96b7e445281a191b3922d814aae420ce.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/Dao.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/EfSDjJnP6oM.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/HypeBears.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/IoCGN_5fC70.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/IolTVnG4GYM.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/M6bulSLTABk.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/Screenshot_3.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/WDArBCihiA4.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/WxBWWMmyGI2ZdQpuNOG0Gg3Z3gaqn3jm9JpGl7RWK2QBbcJL3daBxRCKnOONftiIaQxjL6Y-nEMBhrRLvK3u5Tv4.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/ZgUYfjR_f9I.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/artworks-000538677318-ge5qc6-t500x500.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/bf793252b7cfc2e8103e83af462d8b42.1000x1000x1.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/ea32f6c983edd27255ec982bb9f1fd53.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/hgvOykEhLss.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/hype1.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/hype3.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/i28pbOZL.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/ic-IS90X2EY.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/photo_2022-07-20_09-34-30.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/photo_2022-09-09_10-10-34.jpg",
+  //   "https://ipfs.io/ipfs/QmXSrtqdSS8bgbmYhghhtS1Mgo6CGVV6pYswoshfMhagAG/yrPts-KiI1Y.jpg",
+  // ];
+
+  return (
+    <>
+      {/* {images.map((image, index) => (
+        <img src={image} key={index}></img>
+      ))} */}
+    </>
+  );
 };
---
+++ graph/src/collection.ts
@@ -0,0 +1,42 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import {
+  CollectionFactory,
+  CollectionCreated,
+} from "../generated/CollectionFactory/CollectionFactory";
+import { ExampleEntity } from "../generated/schema";
+
+export function handleCollectionCreated(event: CollectionCreated): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = ExampleEntity.load(event.transaction.from);
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new ExampleEntity(event.transaction.from);
+  }
+
+  // Entity fields can be set based on event parameters
+  entity.collectionAddress = event.params.collectionAddress;
+  entity.creatorAddress = event.params.creatorAddress;
+
+  // Entities can be written to the store with `.save()`
+  entity.save();
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.getUserCollections(...)
+}
